a04da9f61b50add44fc02ffcd7c78bca4964cfdb
sidhanathdon <sid55boss@gmail.com>
2023-12-08 21:59:31 +0530

    coin

--- pages/coins/[coin_id].js
+++ pages/coins/[coin_id].js
@@ -1,20 +1,38 @@
+// import { join } from "cypress/types/bluebird";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
 function CoinDetail() {
   const [coin, setCoin] = useState(null);
 
+  const router =useRouter();
+  const { coin_id }=router.query;
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const res=await fetch(`https://api.coinlore.net/api/ticker/?id=${coin_id}`);
+      const json= await res.json();
+      setCoin(json[0]);
+    }
+    fetchData();
+  },[coin_id]);// only for the coin_id inst
+
+  if(!coin){
+    return <h1>Loading Data...</h1>;
+  }
+
   return (
     <div className="coin-detail">
       <div className="name-symbol">
-        <h1 className="name">coin-name</h1>
-        <h2 className="symbol">(coin-symbol)</h2>
+        <h1 className="name">{coin.name}</h1>
+        <h2 className="symbol">({coin.symbol})</h2>
       </div>
       <div className="market-description">
-        <p className="coin-rank">Rank: coin-symbol</p>
-        <p className="coin-price">Price: $coin-price</p>
-        <p className="coin-market-cap">Market Cap: $coin-market_cap_usd</p>
-        <p className="coin-total-supply">Total Supply: coin-tsupply</p>
-        <p className="coin-max-supply">Max Supply: coin-msupply</p>
+        <p className="coin-rank">Rank: {coin.rank}</p>
+        <p className="coin-price">Price: ${coin.price_usd}</p>
+        <p className="coin-market-cap">Market Cap: ${coin.market_cap_usd}</p>
+        <p className="coin-total-supply">Total Supply: {coin.tsupply}</p>
+        <p className="coin-max-supply">Max Supply: {coin.msupply}</p>
       </div>
     </div>
   );
